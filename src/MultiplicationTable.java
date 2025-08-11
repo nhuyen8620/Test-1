7bd4bae15f90b16715df0881b9132506c09eed5c
DESKTOP-VPSC7IV\OS <nhuyen8620@gmail.com>
2025-08-11 22:41:46 +0700

    Xuân Hùng test 2

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class MultiplicationTable {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in) ;
         System.out.print("Nhập vào số cần in ra bảng cửu chương: ");
+        System.out.print("Nhập vào số tiền: ");
         int n = scanner.nextInt();
         for(int i = 1; i<10; i++){
             i = n * (i+1);
